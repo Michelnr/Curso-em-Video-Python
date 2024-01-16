4f65fc043aadce30d1c24869a13b4108d377f53b
Michel <michelnropke@gmail.com>
2024-01-16 17:48:26 -0300

    atualização do código

--- aula06a.py
+++ aula06a.py
@@ -3,7 +3,7 @@
 #float - para numero quebrados
 #bool - True e False para verdadeira e falso, as letras precisando ser MAIUSCULAS
 
-n1 = int(input('Primeiro n1 '))
-n2 = int(input('Segundo n1 '))
+n1 = int(input('Primeiro Numero: '))
+n2 = int(input('Segundo numero: '))
 soma = n1+n2
 print(f'A soma entre {n1} e {n2} vale {soma}')
