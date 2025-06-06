db42f6d486a13b2a3794595e6205375ea2e5f177
Michel <michelnropke@gmail.com>
2025-06-06 12:09:33 -0300

    update

--- ex084.py
+++ ex084.py
@@ -3,9 +3,18 @@
 #b) Uma listagem com as pessoas mais pesadas.
 #c) uma listagem com as pessoas mais leves.
 #OBS: 100kg ou mais pesado, menos é leve.
+def cadastro_peso(nome, peso):
+    nome = 'Joao' #input('Nome: ')
+    peso = 72.0 #int(input('Peso: '))
+    base_nome = []
+    base_peso = []
+    base_nome.append(nome)
+    base_peso.append(peso)
+        
+while True:
+    nome = input('Informe o nome:')
+    peso = int(input('Informe o peso: '))
+        
+print(maior_peso)
 
-base = []
-pessoa = []
-peso = []
-
-base = input()
+print(f'Foram Cadastradas: {len(base_nome)}')
