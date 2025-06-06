765fed7316a5f7cab7efc22ab1c071ac1fae0379
Michel <michelnropke@gmail.com>
2025-06-06 19:07:23 -0300

    update

--- mundo_3/ex084.py
+++ mundo_3/ex084.py
@@ -3,18 +3,17 @@
 #b) Uma listagem com as pessoas mais pesadas.
 #c) uma listagem com as pessoas mais leves.
 #OBS: 100kg ou mais pesado, menos é leve.
-def cadastro_peso(nome, peso):
-    nome = 'Joao' #input('Nome: ')
-    peso = 72.0 #int(input('Peso: '))
+
+def cadastro_peso():
     base_nome = []
     base_peso = []
     base_nome.append(nome)
     base_peso.append(peso)
-        
-while True:
-    nome = input('Informe o nome:')
-    peso = int(input('Informe o peso: '))
-        
-print(maior_peso)
 
-print(f'Foram Cadastradas: {len(base_nome)}')
+    while True:
+        nome_pessoa = input('Informe o nome:')
+        peso_pessoa = int(input('Informe o peso: '))
+        if nome_pessoa == "sair":
+            break
+
+    for nome in base_nome
