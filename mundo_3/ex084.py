efd6f9825d97c096933e66a7d192f97b87518712
Michel <michelnropke@gmail.com>
2025-06-07 22:35:03 -0300

    ex084 finalizado começando ex085

--- mundo_3/ex084.py
+++ mundo_3/ex084.py
@@ -4,16 +4,23 @@
 #c) uma listagem com as pessoas mais leves.
 #OBS: 100kg ou mais pesado, menos é leve.
 
-def cadastro_peso():
-    base_nome = []
-    base_peso = []
-    base_nome.append(nome)
-    base_peso.append(peso)
+base_nome_peso = {}
 
-    while True:
-        nome_pessoa = input('Informe o nome:')
-        peso_pessoa = int(input('Informe o peso: '))
-        if nome_pessoa == "sair":
-            break
+while True:
+    print('Para finalizar digite "sair"')
+    nome_pessoa = input('Informe o nome: ').title()
+    if nome_pessoa == "S":
+        break
+    peso_pessoa = float(input('Informe o peso: '))
 
-    for nome in base_nome
+    base_nome_peso[nome_pessoa] = peso_pessoa
+
+print(f'\nForam cadastradas {len(base_nome_peso)} pessoas:')
+print(f'Pessoas com mais de 100kg:')
+for cadastro in base_nome_peso: #Pessoas mais pesadas
+    if base_nome_peso.get(cadastro) >= 100:
+        print(f'{cadastro} - {base_nome_peso.get(cadastro)}Kg')
+print(f'\nPessoas com menos de 100kg:')
+for cadastro in base_nome_peso:  # Pessoas menos pesadas
+    if base_nome_peso.get(cadastro) < 100:
+        print(f'{cadastro} - {base_nome_peso.get(cadastro)}Kg')
