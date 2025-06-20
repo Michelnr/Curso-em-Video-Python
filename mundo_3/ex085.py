c63bb41016bae48a4ddcac1d5e584ff1a552f8bc
Michel <michelnropke@gmail.com>
2025-06-20 11:37:23 -0300

    ex085 = finalizado

--- mundo_3/ex085.py
+++ mundo_3/ex085.py
@@ -1,17 +1,18 @@
 """Exercício Python 085: Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista
 única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente."""
+from random import randint
 
 lista_numero = [[], []] #0 = PAR, 1 = IMPAR
-cont = 0
-while True:
-    if cont == 7:
-        break
-    numero = int(input(f"Informe o numero {cont+1}º número: "))
-    #numero = (randint(1, 100))
+for c in range(1,8):
+    #numero = int(input(f"Informe o numero {c}º número: "))
+    numero = (randint(1, 100))
     if numero % 2 == 0:
         lista_numero[0].append(numero)
     else:
         lista_numero[1].append(numero)
-    cont += 1
-print(f'Números pares -----> {sorted(lista_numero[0])}')
-print(f'Números Impares ---> {sorted(lista_numero[1])}')
+
+lista_numero[0].sort()
+lista_numero[1].sort()
+
+print(f'Números pares -----> {lista_numero[0]}')
+print(f'Números Impares ---> {lista_numero[1]}')
