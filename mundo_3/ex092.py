b9211fa784c726d2395f3977c7d7907930c8659c
Michel <michelnropke@gmail.com>
2025-07-17 19:09:37 -0300

    em progresso
    Signed-off-by: Michel <michelnropke@gmail.com>

---
+++ mundo_3/ex092.py
@@ -0,0 +1,9 @@
+# Exercício Python 092: Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o (com idade) em um dicionário. 
+nome = str(input('Informe o nome: '))
+ano_nas = int(input('Informe seu ano de nascimento: '))
+carteira_ctps = int(input('Informe o numero da CTPS'))
+
+# Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. 
+
+
+# Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
