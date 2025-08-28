c016a8190ef7bd050f5c7abafed264a5a80c56b4
Michel <michelnropke@gmail.com>
2025-08-27 23:11:28 -0300

    ex091.py = em produção

--- mundo_3/ex091.py
+++ mundo_3/ex091.py
@@ -7,12 +7,16 @@ resultados = {}
 
 # Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios.
 # Guarde esses resultados em um dicionário em Python.
-j = 0
-while j < 4:
-    j += 1
-    resultados[f'Jogador-{j}'] = randint(1,10)
+dados_quantidade = 0
+dados_resultado = 0
 
+while dados_quantidade < 4:
+    dados_resultado = randint(1,6)
+    if dados_resultado not in resultados.values():
+        dados_quantidade += 1
+        resultados[f'Jogador {dados_quantidade}'] = dados_resultado
 
+print(resultados)
 # No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 
 ####### ASSISTIR A AULA #########
