4837c0829cb43e2fabb090200477ff2be2ef8a62
Michel <148887903+Michelnr@users.noreply.github.com>
2025-10-24 16:05:37 +0000

    aula20 feita

---
+++ mundo_3/aula20-funções.py
@@ -0,0 +1,26 @@
+'''Nessa aula, vamos aprender o que são funções ou rotinas e como utilizar funções em Python. 
+Funções são trechos de código que podem ser executados em momentos diferentes de nossos códigos em Python. 
+Veja como funciona o comando def em Python e como utilizá-lo com parâmetros simples e múltiplos.'''
+
+def mostrarLinha():
+    print(10*'---')
+
+def mensagem(msg):
+    print(10*'---')
+    print({msg})
+    print(10*'---')
+
+def soma(a, b):
+    s = a + b
+    print(f'Soma de A + B = {s}')
+
+def contador(*num):
+
+
+mostrarLinha()
+mensagem('Sistema de Alunos')
+soma(2, 3)
+soma(a=2, b=3)
+contador(5, 7, 1)
+contador(4, 9, 1, 5, 3)
+contador(7, 9, 5, 8, 4, 7, 3, 9)
