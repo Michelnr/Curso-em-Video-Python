a170ad50bf3a2e47e8133f387bf7e515471d4e10
Michel <michelnropke@gmail.com>
2025-11-29 16:05:06 -0300

    ex101.py

--- mundo_3/ex101.py
+++ mundo_3/ex101.py
@@ -1,5 +1,5 @@
 # Exercício Python 101: Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal 
-# indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições. 
+# indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
 def voto(ano_nasci):
     from datetime import date
     '''
