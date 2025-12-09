a1581d6b58d997498af7aca6bb8f134347d33313
Michel <michelnropke@gmail.com>
2025-12-09 19:54:08 -0300

    ex108.py = finalizado

--- mundo_3/ex108.py
+++ mundo_3/ex108.py
@@ -1,25 +1,17 @@
 # Exercício Python 108: Adapte o código do desafio #107, criando uma função adicional chamada moeda() que
 # consiga mostrar os números como um valor monetário formatado.
-from modulos import numeros
+from modulos.numeros import matematica
 
 valor_moeda = float(input('Informe o valor: R$'))
 
 # aumento o valor em 10%
-numeros.aumentar10(valor_moeda)
-print(valor_moeda)
-print(f'{numeros.formatoMoeda(valor_moeda)} + 10% = {numeros.formatoMoeda(valor_moeda_mais10)}')
+print(f'{matematica.formatoMoeda(valor_moeda)} + 10% = {matematica.formatoMoeda(matematica.aumentar10(valor_moeda))}')
 
 # diminui o valor em 10%
-valor_moeda_menos10 = valor_moeda
-numeros.diminui10(valor_moeda_menos10)
-print(f'{valor_moeda} - 10% = {numeros.diminui10(valor_moeda_menos10):.2f}')
+print(f'{matematica.formatoMoeda(valor_moeda)} - 10% = {matematica.formatoMoeda(matematica.diminui10(valor_moeda))}')
 
 # dobre o valor
-valor_moeda_dobro = valor_moeda
-numeros.dobro(valor_moeda_dobro)
-print(f'{valor_moeda} * 2 = {numeros.dobro(valor_moeda_dobro):.2f}')
+print(f'{matematica.formatoMoeda(valor_moeda)} X 2 = {matematica.formatoMoeda(matematica.dobro(valor_moeda))}')
 
 # divide o valor por 2
-valor_moeda_metade = valor_moeda
-numeros.metade(valor_moeda_metade)
-print(f'{valor_moeda} / 2 = {numeros.metade(valor_moeda_metade):.2f}')
+print(f'{matematica.formatoMoeda(valor_moeda)} % 2 = {matematica.formatoMoeda(matematica.metade(valor_moeda))}')
--- teste.py
+++ teste.py
@@ -1,3 +1,7 @@
-from modulos.numeros import formatoMoeda
+from modulos.numeros import matematica
 
-formatoMoeda(10.5)
+valor_moeda = float(input('Informe o valor: R$'))
+
+# aumento o valor em 10%
+print(matematica.formatoMoeda(matematica.aumentar10(valor_moeda)))
+#print(f'{matematica.formatoMoeda(valor_moeda)} + 10% = {matematica.aumentar10(valor_moeda)}')
