45c7a50ead4a10aba7ed21d49b3ffafdf7ec94ce
Michel <148887903+Michelnr@users.noreply.github.com>
2025-12-15 13:03:54 +0000

    corrigindo ex112.py e leiaDinheiro.py

--- ex112.py
+++ ex112.py
@@ -1,8 +1,8 @@
 # Exercício Python 112: Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo chamado dado. 
 # Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como a função imputa(), mas com uma validação de dados para aceitar apenas valores que seja monetários.
 
-from modulos.numeros import moeda
-from modulos.dados import leiaDinheiro
+from modulos.numeros.moeda import resumo
+from modulos.dados.leiaDinheiro import leiaDinheiro
 
 p = leiaDinheiro('Digite o preço: R$')
-moeda.resumo(p, 35, 22)
+resumo(p, 35, 22)
